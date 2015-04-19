fcd4e203d2676f9570c6c3836e18edc26848e0fb
Gavin Dinubilo <Gavin@gavins-air.attlocal.net>
2015-04-19 16:34:50 -0700

    Please merge

--- javascripts/the.js
+++ javascripts/the.js
@@ -4,6 +4,10 @@ var socket = io.connect('http://localhost:3000');
 
 currentTrack = new Audio();
 
+var nowPlayingImage = document.getElementById("nowPlayingImage");
+var nowPlayingInfo = document.getElementById("nowPlayingInfo");
+
+
 var commentForm = document.getElementById("comment");
 commentForm.onsubmit = function(e) {
 	var comment = document.getElementById("commentText");
@@ -20,19 +24,33 @@ trackForm.onsubmit = function(e) {
 	var track = document.getElementById("url");
 	resolve("6e73cbbe5e7edeba75334acbeb81492f", track.value, function(err, body, stream_url) {
 		console.log(err);
-		if (err) return false;
+		if (err || body.kind == 'playlist' || body.streamable == false) {
+			comment("Sorry that track is not playable!");
+			return false;
+		}
 		socket.emit('add track', body);
 	});
+	track.value = '';
 	return false;
 }
 
 socket.on('comment', function(data) {
 	var text = data;
-	var comments = document.getElementById("comments");
-	comments.innerHTML = comments.innerHTML + '<br />' + text;
+	comment(text);
 });
 
+function comment(text) {
+	var comments = document.getElementById("comments");
+	comments.innerHTML = text + "<br />" + comments.innerHTML;
+}
+
 socket.on('set audio player', function(data) {
+	console.log(data.soundcloud);
+	var soundcloudInfo = data.soundcloud;
+
+	nowPlayingImage.src = soundcloudInfo.artwork_url;
+	nowPlayingInfo.innerText = soundcloudInfo.title;
+
 	currentTrack.src = data.stream;
 	currentTrack.currentTime = data.currentTime / 1000;
 	currentTrack.play();
